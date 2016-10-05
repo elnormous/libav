4935e75c1dd358566af44ce36da310e8affd239e
elnormous <elviss@elviss.lv>
2016-10-05 17:32:13 +0300

    Store the count of dropped frames

--- libavdevice/bmd_dec.c
+++ libavdevice/bmd_dec.c
@@ -80,6 +80,7 @@ static void packet_queue_end(PacketQueue *q)
 
 static int packet_queue_put(PacketQueue *q, AVPacket *pkt)
 {
+    int ret = 0;
     pthread_mutex_lock(&q->mutex);
 
     if (q->nb_packets <= MAX_QUEUE_SIZE) {
@@ -87,6 +88,7 @@ static int packet_queue_put(PacketQueue *q, AVPacket *pkt)
 
         pkt_entry = (AVPacketList *)av_malloc(sizeof(AVPacketList));
         if (!pkt_entry) {
+            pthread_mutex_unlock(&q->mutex);
             return AVERROR(ENOMEM);
         }
         pkt_entry->pkt  = *pkt;
@@ -105,6 +107,7 @@ static int packet_queue_put(PacketQueue *q, AVPacket *pkt)
     }
     else {
         av_packet_unref(pkt);
+        return AVERROR(ENOBUFS);
     }
 
     pthread_mutex_unlock(&q->mutex);
@@ -287,7 +290,11 @@ static int video_callback(void *priv, uint8_t *frame,
     pkt.flags        |= AV_PKT_FLAG_KEY;
     pkt.stream_index  = ctx->video_st->index;
 
-    return packet_queue_put(&ctx->q, &pkt);
+    if (packet_queue_put(&ctx->q, &pkt) != 0) {
+        ctx->video_st->codec->dropped_frames++;
+    }
+
+    return 0;
 }
 
 static int audio_callback(void *priv, uint8_t *frame,
@@ -313,7 +320,11 @@ static int audio_callback(void *priv, uint8_t *frame,
     pkt.flags        |= AV_PKT_FLAG_KEY;
     pkt.stream_index  = ctx->audio_st->index;
 
-    return packet_queue_put(&ctx->q, &pkt);
+    if (packet_queue_put(&ctx->q, &pkt) != 0) {
+        ctx->audio_st->codec->dropped_frames++;
+    }
+
+    return 0;
 }
 
 static int bmd_read_header(AVFormatContext *s)
