c10d682473c5521c0cbce2d67274f7d84cce0ff8
Elviss Strazdins <elviss@elviss.lv>
2017-11-14 14:44:45 +0200

    Remove the RabbitMQ

--- libavdevice/audiometer.c
+++ libavdevice/audiometer.c
@@ -7,16 +7,11 @@
 #include "libavutil/internal.h"
 
 #include "libavformat/avformat.h"
-#include <amqp_tcp_socket.h>
-#include <amqp.h>
-#include <amqp_framing.h>
 
 typedef struct AudioMeterContext {
     AVClass *class;
     int channels;
     AVCodecContext *avctx;
-    amqp_socket_t *socket;
-    amqp_connection_state_t conn;
 } AudioMeterContext;
 
 static av_cold int audiometer_write_header(AVFormatContext *s1)
@@ -38,14 +33,6 @@ static av_cold int audiometer_write_header(AVFormatContext *s1)
         goto fail;
     }
 
-    s->conn = amqp_new_connection();
-    s->socket = amqp_tcp_socket_new(s->conn);
-    if (!s->socket) {
-        av_log(s1, AV_LOG_ERROR,
-               "failed to connect to RabbitMQ\n");
-        goto fail;
-    }
-
     s1->filename;
 
     return res;
@@ -62,7 +49,7 @@ static int audiometer_write_packet(AVFormatContext *s1, AVPacket *pkt)
     int size     = pkt->size;
     uint8_t *buf = pkt->data;
 
-    size /= s->frame_size;
+    /*size /= s->frame_size;
     if (s->reorder_func) {
         if (size > s->reorder_buf_size)
             //if (reorder(s, size))
@@ -70,27 +57,25 @@ static int audiometer_write_packet(AVFormatContext *s1, AVPacket *pkt)
                 return AVERROR(ENOMEM);
         s->reorder_func(buf, s->reorder_buf, size);
         buf = s->reorder_buf;
-    }
-    //while ((res = write(s->h, buf, size)) < 0) {
-    while (0) { // TODO: write to RabbitMQ
-    }
+    }*/
 
     return 0;
 }
 
 static int audiometer_close()
 {
-    // TODO: close RabbitMQ connection
+    // TODO: close connection
+    return 0;
 }
 
 AVOutputFormat ff_audiometer_muxer = {
     .name           = "audiometer",
     .long_name      = NULL_IF_CONFIG_SMALL("Audio meter"),
     .priv_data_size = sizeof(AudioMeterContext),
-    .audio_codec    = DEFAULT_CODEC_ID,
+    .audio_codec    = AV_CODEC_ID_PCM_S16BE, AV_CODEC_ID_PCM_S16LE, // accept only PCM data
     .video_codec    = AV_CODEC_ID_NONE,
     .write_header   = audiometer_write_header,
     .write_packet   = audiometer_write_packet,
     .write_trailer  = audiometer_close,
-    .flags          = AVFMT_NOFILE,
+    .flags          = AVFMT_NOFILE
 };
